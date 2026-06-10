f643e79776042d7a8af58cd7446089b7995a21c8
Emam Al Mounqer <Emam.Almounqer@leading-point.com>
2026-06-10 17:32:44 +0300

    remove db log

--- SematiNotificationAutoFix.Console/Extensions/LoggingExtensions.cs
+++ SematiNotificationAutoFix.Console/Extensions/LoggingExtensions.cs
@@ -10,27 +10,33 @@ public static class LoggingExtensions
 {
     public static IHostApplicationBuilder AddSerilogLogging(this IHostApplicationBuilder builder)
     {
-        var columnOptions = new ColumnOptions();
-        columnOptions.AdditionalColumns =
-        [
-            new SqlColumn { ColumnName = "ActionId",    DataType = SqlDbType.Int,      AllowNull = true },
-            new SqlColumn { ColumnName = "Tcn",         DataType = SqlDbType.NVarChar, DataLength = 100, AllowNull = true },
-            new SqlColumn { ColumnName = "PersonId",    DataType = SqlDbType.NVarChar, DataLength = 50,  AllowNull = true },
-            new SqlColumn { ColumnName = "ProcessName", DataType = SqlDbType.NVarChar, DataLength = 50,  AllowNull = true },
-        ];
 
-        Log.Logger = new LoggerConfiguration()
+
+        var config = new LoggerConfiguration()
             .ReadFrom.Configuration(builder.Configuration)
-            .Enrich.FromLogContext()
-            .WriteTo.MSSqlServer(
-                connectionString: builder.Configuration.GetConnectionString("Logs"),
-                sinkOptions: new MSSqlServerSinkOptions
-                {
-                    TableName = "Logs",
-                    AutoCreateSqlTable = true
-                },
-                columnOptions: columnOptions)
-            .CreateLogger();
+            .Enrich.FromLogContext();
+
+        // -- db log confgi --
+        // var columnOptions = new ColumnOptions();
+        // // columnOptions.Store.Clear();
+        // columnOptions.AdditionalColumns =
+        // [
+        //     new SqlColumn { ColumnName = "ActionId",    DataType = SqlDbType.Int,      AllowNull = true },
+        //     new SqlColumn { ColumnName = "Tcn",         DataType = SqlDbType.NVarChar, DataLength = 100, AllowNull = true },
+        //     new SqlColumn { ColumnName = "PersonId",    DataType = SqlDbType.NVarChar, DataLength = 50,  AllowNull = true },
+        //     new SqlColumn { ColumnName = "ProcessName", DataType = SqlDbType.NVarChar, DataLength = 50,  AllowNull = true },
+        // ];
+
+        //config.WriteTo.MSSqlServer(
+        //    connectionString: builder.Configuration.GetConnectionString("Logs"),
+        //    sinkOptions: new MSSqlServerSinkOptions
+        //    {
+        //        TableName = "Logs",
+        //        AutoCreateSqlTable = false
+        //    },
+        //    columnOptions: columnOptions);
+
+        Log.Logger = config.CreateLogger();
 
         builder.Logging.AddSerilog();
 
